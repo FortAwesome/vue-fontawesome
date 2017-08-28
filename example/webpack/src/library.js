03e971872fcbc0aa90ed89cfa110971b6a903b19
Travis Chase <travis@travischase.me>
2017-08-28 12:55:00 -0500

    Adding the rest of the CSS features

--- example/webpack/src/library.js
+++ example/webpack/src/library.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import App from './library/App.vue'
 import fontawesome from '@fortawesome/fontawesome'
 import brands from '@fortawesome/fontawesome-brands'
-import { faCoffee, faCog } from '@fortawesome/fontawesome-solid'
+import { faCoffee, faCog, faSpinner, faQuoteLeft, faSquare, faCheckSquare } from '@fortawesome/fontawesome-solid'
 
-fontawesome.library.add(brands, faCoffee, faCog)
+fontawesome.library.add(brands, faCoffee, faCog, faSpinner, faQuoteLeft, faSquare, faCheckSquare)
 
 new Vue({
   el: '#app',
