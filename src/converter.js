5332ef795f63dbd63c6db0d9dc8f90099d48b74d
Hosmel Quintana <hosmelq@gmail.com>
2017-10-03 23:30:21 -0600

    use humps instead of camelcase

--- src/converter.js
+++ src/converter.js
@@ -1,4 +1,4 @@
-import camelCase from 'camelcase'
+import humps from 'humps';
 
 function styleToObject (style) {
   return style.split(';')
@@ -6,7 +6,7 @@ function styleToObject (style) {
     .filter(s => s)
     .reduce((acc, pair) => {
       const i = pair.indexOf(':')
-      const prop = camelCase(pair.slice(0, i))
+      const prop = humps.camelize(pair.slice(0, i))
       const value = pair.slice(i + 1).trim()
       
       acc[prop] = value
