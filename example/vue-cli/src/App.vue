7afb2b5c807c8c8479a7cb729e14bb8ab8e6d8df
robmadole <robmadole@gmail.com>
2018-03-12 11:24:31 -0500

    Resetting the example

--- example/vue-cli/src/App.vue
+++ example/vue-cli/src/App.vue
@@ -1,25 +1,24 @@
 <template>
   <div id="app">
-    <h1>
-      <font-awesome-icon :icon="['fas', 'coffee']" />
-    </h1>
+    <img src="./assets/logo.png">
+    <HelloWorld/>
   </div>
 </template>
 
 <script>
-import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
+import HelloWorld from './components/HelloWorld'
 
 export default {
-  name: 'FAExternalExample',
-
+  name: 'App',
   components: {
-    FontAwesomeIcon
+    HelloWorld
   }
 }
 </script>
 
 <style>
 #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
