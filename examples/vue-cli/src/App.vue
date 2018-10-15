0a63cab8bfd0ad1358ace29c60f5872cb24d155f
robmadole <robmadole@gmail.com>
2018-10-14 20:05:25 -0500

    Update deps

--- examples/vue-cli/src/App.vue
+++ examples/vue-cli/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <main class="w-100 min-vh-100 bg-gray8 white sans-serif pa6 flex flex-column justify-center items-center">
       <div class="mw8 center overflow-hidden">
-        <h2 class="tc ttu tracked3 b f2 mt0 mb2 teal0 o-30">react-fontawesome</h2>
+        <h2 class="tc ttu tracked3 b f2 mt0 mb2 teal0 o-30">vue-fontawesome</h2>
 
         <ul class="list ma0 pa0 flex flex-row flex-wrap teal4">
           <li class="pv3 ph2 ma0 link grow">
