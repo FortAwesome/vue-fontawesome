9cfd1aa4a4fb62236b4b78d593b78ba747f021a2
Liu Xinyu <meteor.lxy@foxmail.com>
2018-12-04 22:47:19 +0800

    Add types declaration file (#159)

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+declare module '@fortawesome/vue-fontawesome' {
+  import { FunctionalComponentOptions } from 'vue'
+  export const FontAwesomeIcon: FunctionalComponentOptions
+  export const FontAwesomeLayers: FunctionalComponentOptions
+  export const FontAwesomeLayersText: FunctionalComponentOptions
+}
