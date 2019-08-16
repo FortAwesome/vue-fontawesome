461aaabccd6295308e19a4b2a09a550007f9e430
Bene Tamás <btaens@hotmail.com>
2019-08-16 09:16:43 +0200

    Fixed typings

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,8 @@
-declare module '@fortawesome/vue-fontawesome' {
-  import { FunctionalComponentOptions } from 'vue'
-  import { PropsDefinition } from 'vue/types/options'
-  interface FontAwesomeIconProps { }
-  interface FontAwesomeLayersProps { }
-  interface FontAwesomeLayersTextProps { }
-  export const FontAwesomeIcon: FunctionalComponentOptions<FontAwesomeIconProps, PropsDefinition<FontAwesomeIconProps>>
-  export const FontAwesomeLayers: FunctionalComponentOptions<FontAwesomeLayersProps, PropsDefinition<FontAwesomeLayersProps>>
-  export const FontAwesomeLayersText: FunctionalComponentOptions<FontAwesomeLayersTextProps, PropsDefinition<FontAwesomeLayersTextProps>>
-}
+import { FunctionalComponentOptions } from 'vue'
+import { PropsDefinition } from 'vue/types/options'
+interface FontAwesomeIconProps { }
+interface FontAwesomeLayersProps { }
+interface FontAwesomeLayersTextProps { }
+export const FontAwesomeIcon: FunctionalComponentOptions<FontAwesomeIconProps, PropsDefinition<FontAwesomeIconProps>>
+export const FontAwesomeLayers: FunctionalComponentOptions<FontAwesomeLayersProps, PropsDefinition<FontAwesomeLayersProps>>
+export const FontAwesomeLayersText: FunctionalComponentOptions<FontAwesomeLayersTextProps, PropsDefinition<FontAwesomeLayersTextProps>>
