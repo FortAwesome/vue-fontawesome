d08dce566f4b0249def014985976b907332db8f4
Tyranteon <git@tyranteon.com>
2020-06-15 09:44:24 +0200

    Add TSX support to Typescript definition

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
-import { FunctionalComponentOptions } from 'vue'
+import { FunctionalComponentOptions, VueConstructor } from 'vue'
 import { PropsDefinition } from 'vue/types/options'
 interface FontAwesomeIconProps { }
 interface FontAwesomeLayersProps { }
 interface FontAwesomeLayersTextProps { }
-export const FontAwesomeIcon: FunctionalComponentOptions<FontAwesomeIconProps, PropsDefinition<FontAwesomeIconProps>>
+export const FontAwesomeIcon: FunctionalComponentOptions<FontAwesomeIconProps, PropsDefinition<FontAwesomeIconProps>> & VueConstructor
 export const FontAwesomeLayers: FunctionalComponentOptions<FontAwesomeLayersProps, PropsDefinition<FontAwesomeLayersProps>>
 export const FontAwesomeLayersText: FunctionalComponentOptions<FontAwesomeLayersTextProps, PropsDefinition<FontAwesomeLayersTextProps>>
