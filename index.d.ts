4e9dfdde7146b5850d1f2a2230894e6a80d4981a
rigma <rigbuntu@gmail.com>
2020-08-20 13:43:24 +0200

    chore: repacking vue-awesome package
    chore: updating typescript typings

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,35 @@
-import { FunctionalComponentOptions, VueConstructor } from 'vue'
-import { PropsDefinition } from 'vue/types/options'
-interface FontAwesomeIconProps { }
-interface FontAwesomeLayersProps { }
-interface FontAwesomeLayersTextProps { }
-export const FontAwesomeIcon: FunctionalComponentOptions<FontAwesomeIconProps, PropsDefinition<FontAwesomeIconProps>> & VueConstructor
-export const FontAwesomeLayers: FunctionalComponentOptions<FontAwesomeLayersProps, PropsDefinition<FontAwesomeLayersProps>>
-export const FontAwesomeLayersText: FunctionalComponentOptions<FontAwesomeLayersTextProps, PropsDefinition<FontAwesomeLayersTextProps>>
+import { ComponentPublicInstance } from 'vue'
+
+interface FontAwesomeIconProps {
+  border: boolean
+  fixedWidth: boolean
+  flip: 'horizontal' | 'vertical' | 'both' | null
+  icon: object | Array<string> | string
+  mask: object | Array<string> | string | null
+  listItem: boolean
+  pull: 'right' | 'left' | null
+  pulse: boolean
+  rotation: 90 | 180 | 270 | '90' | '180' | '270' | null
+  swapOpacity: boolean
+  size: 'lg' | 'xs' | 'sm' | '1x' | '2x' | '3x' | '4x' | '5x' | '6x' | '7x' | '8x' | '9x' | '10x' | null
+  spin: boolean
+  transform: object | string | null
+  symbol: boolean | string
+  title: string | null
+  inverse: boolean
+}
+
+interface FontAwesomeLayersProps {
+  fixedWidth: boolean
+}
+
+interface FontAwesomeLayersTextProps {
+  value: string | number
+  transform: object | string | null
+  counter: boolean
+  position: 'bottom-left' | 'bottom-right' | 'top-left' | 'top-right' | null
+}
+
+declare const FontAwesomeIcon: ComponentPublicInstance<FontAwesomeIconProps>
+declare const FontAwesomeLayers: ComponentPublicInstance<FontAwesomeLayersProps>
+declare const FontAwesomeLayersText: ComponentPublicInstance<FontAwesomeLayersTextProps>
