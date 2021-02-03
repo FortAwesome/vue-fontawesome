e38c1710376180b3a9a2c403c2007976407cce8b
Restyled.io <commits@restyled.io>
2021-02-03 05:31:01 +0000

    Restyled by prettier

--- src/components/__fixtures__/helpers.js
+++ src/components/__fixtures__/helpers.js
@@ -1,34 +1,37 @@
-import {parse} from '@fortawesome/fontawesome-svg-core'
-import Vue from 'vue/dist/vue'
+import { parse } from "@fortawesome/fontawesome-svg-core";
+import Vue from "vue/dist/vue";
 
-import FontAwesomeIcon from '../FontAwesomeIcon'
+import FontAwesomeIcon from "../FontAwesomeIcon";
 
 export function compileAndMount(str, params = {}) {
-  const res = Vue.compile(str)
-  const vm = new Vue(
-      {...params, render : res.render, staticRenderFn : res.staticRenderFn})
+  const res = Vue.compile(str);
+  const vm = new Vue({
+    ...params,
+    render: res.render,
+    staticRenderFn: res.staticRenderFn,
+  });
 
-  vm.$mount()
+  vm.$mount();
 
-  return vm
+  return vm;
 }
 
 export function mountFromProps(propsData = {}) {
   const opts = {
-    render : (h) => h(FontAwesomeIcon, {props : propsData})
-  }
+    render: (h) => h(FontAwesomeIcon, { props: propsData }),
+  };
 
-  const vm = new Vue(opts)
-  vm.$mount()
+  const vm = new Vue(opts);
+  vm.$mount();
 
-  return vm
+  return vm;
 }
 
 export function coreHasFeature(feature) {
   if (feature === REFERENCE_ICON_BY_STYLE || feature === ICON_ALIASES) {
-    return parse.icon
+    return parse.icon;
   }
 }
 
-export const REFERENCE_ICON_BY_STYLE = 0x00
-export const ICON_ALIASES = 0x01
+export const REFERENCE_ICON_BY_STYLE = 0x00;
+export const ICON_ALIASES = 0x01;
