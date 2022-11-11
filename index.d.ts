703f69f6135adaf531d536b4a3ac99fd7f33ec14
Brandon Mork <brandon.mork@abrigo.com>
2022-11-11 13:03:52 -0500

    Issue #415 Sync types in index.d.ts FontAwesomeIconProps to match actual expected props in FontAwesomeIcon.js.

--- index.d.ts
+++ index.d.ts
@@ -12,12 +12,20 @@ interface FontAwesomeIconProps {
   pulse?: boolean
   rotation?: 90 | 180 | 270 | '90' | '180' | '270'
   swapOpacity?: boolean
-  size?: 'lg' | 'xs' | 'sm' | '1x' | '2x' | '3x' | '4x' | '5x' | '6x' | '7x' | '8x' | '9x' | '10x'
+  size?: '2xs' | 'xs' | 'sm' | 'lg' | 'xl' | '2xl' | '1x' | '2x' | '3x' | '4x' | '5x' | '6x' | '7x' | '8x' | '9x' | '10x'
   spin?: boolean
   transform?: object | string
   symbol?: boolean | string
   title?: string
   inverse?: boolean
+  bounce?: boolean
+  shake?: boolean
+  beat?: boolean
+  fade?: boolean
+  beatFade?: boolean
+  flash?: boolean
+  spinPulse?: boolean
+  spinReverse?: boolean
 }
 
 interface FontAwesomeLayersProps {
