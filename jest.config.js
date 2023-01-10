fa769411d848b2dfcf69a74e90ac2e0948fb0a3e
jason <jason@fortawesome.com>
2023-01-10 10:51:57 -0600

    add jest config and update deps

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  verbose: true,
+  moduleFileExtensions: ["js", "vue"],
+  transform: {
+    "^.+\\.js$": "babel-jest",
+    ".*\\.vue$": "<rootDir>/node_modules/@vue/vue3-jest",
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  testEnvironment: "jsdom",
+  testEnvironmentOptions: {
+    customExportConditions: ["node", "node-addons"],
+ },
+};
