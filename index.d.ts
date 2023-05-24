35abcf13992975573432df64bd4b4da0671e3b35
Shige Fukushima <shigeyf@outlook.com>
2023-05-25 01:02:20 +0900

    Update index.d.ts to add export declaration.
    This fix will resolve a problem caused by ESlint.
    
    > error  FontAwesomeIcon not found in '@fortawesome/vue-fontawesome'  import/named

--- index.d.ts
+++ index.d.ts
@@ -41,3 +41,5 @@ interface FontAwesomeLayersTextProps {
 declare const FontAwesomeIcon: DefineComponent<FontAwesomeIconProps>
 declare const FontAwesomeLayers: DefineComponent<FontAwesomeLayersProps>
 declare const FontAwesomeLayersText: DefineComponent<FontAwesomeLayersTextProps>
+
+export { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText }
