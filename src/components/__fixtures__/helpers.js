43bd86e9a67516a31577d3f486a9bd589fb0157f
jason <jason@fortawesome.com>
2023-07-10 14:50:55 -0500

    fixup commit deleted from 3.x

--- src/components/__fixtures__/helpers.js
+++ src/components/__fixtures__/helpers.js
@@ -2,16 +2,16 @@ import { mount } from '@vue/test-utils'
 import FontAwesomeIcon from '../FontAwesomeIcon'
 import { parse } from '@fortawesome/fontawesome-svg-core'
 
-export function compileAndMount (definition, props = {}) {
+export function compileAndMount(definition, props = {}) {
   return mount(definition, { props })
 }
 
-export function mountFromProps (props = {}) {
+export function mountFromProps(props = {}) {
   return mount(FontAwesomeIcon, { props })
 }
 
-export function coreHasFeature (feature) {
-  if (feature === REFERENCE_ICON_BY_STYLE || feature === ICON_ALIASES || feature === REFERENCE_ICON_USING_STRING) {
+export function coreHasFeature(feature) {
+  if (feature === REFERENCE_ICON_BY_STYLE || feature === ICON_ALIASES || feature === REFERENCE_ICON_USING_STRING || feature === REFERENCE_ICON_USING_FAMILY) {
     return parse.icon
   }
 }
@@ -19,3 +19,4 @@ export function coreHasFeature (feature) {
 export const REFERENCE_ICON_BY_STYLE = 0x00
 export const ICON_ALIASES = 0x01
 export const REFERENCE_ICON_USING_STRING = 0x02
+export const REFERENCE_ICON_USING_FAMILY = 0x03
