574e930663dd466f2fd7bb02ed546cd4f38e1ee3
jason <jason@fortawesome.com>
2025-04-24 13:07:32 -0500

    fix up - add index.es.js and index.js

--- index.es.js
+++ index.es.js
@@ -1,5 +1,5 @@
-import { parse as parse$7, icon, config, text } from '@fortawesome/fontawesome-svg-core';
 import { h, defineComponent, computed, watch } from 'vue';
+import { parse as parse$7, icon, config, text } from '@fortawesome/fontawesome-svg-core';
 
 function _arrayLikeToArray(r, a) {
   (null == a || a > r.length) && (a = r.length);
@@ -1026,46 +1026,54 @@ const coerce$1 = (version, options) => {
 };
 var coerce_1 = coerce$1;
 
-class LRUCache {
-  constructor () {
-    this.max = 1000;
-    this.map = new Map();
-  }
+var lrucache;
+var hasRequiredLrucache;
 
-  get (key) {
-    const value = this.map.get(key);
-    if (value === undefined) {
-      return undefined
-    } else {
-      // Remove the key from the map and add it to the end
-      this.map.delete(key);
-      this.map.set(key, value);
-      return value
-    }
-  }
+function requireLrucache () {
+	if (hasRequiredLrucache) return lrucache;
+	hasRequiredLrucache = 1;
+	class LRUCache {
+	  constructor () {
+	    this.max = 1000;
+	    this.map = new Map();
+	  }
 
-  delete (key) {
-    return this.map.delete(key)
-  }
+	  get (key) {
+	    const value = this.map.get(key);
+	    if (value === undefined) {
+	      return undefined
+	    } else {
+	      // Remove the key from the map and add it to the end
+	      this.map.delete(key);
+	      this.map.set(key, value);
+	      return value
+	    }
+	  }
 
-  set (key, value) {
-    const deleted = this.delete(key);
+	  delete (key) {
+	    return this.map.delete(key)
+	  }
 
-    if (!deleted && value !== undefined) {
-      // If cache is full, delete the least recently used item
-      if (this.map.size >= this.max) {
-        const firstKey = this.map.keys().next().value;
-        this.delete(firstKey);
-      }
+	  set (key, value) {
+	    const deleted = this.delete(key);
 
-      this.map.set(key, value);
-    }
+	    if (!deleted && value !== undefined) {
+	      // If cache is full, delete the least recently used item
+	      if (this.map.size >= this.max) {
+	        const firstKey = this.map.keys().next().value;
+	        this.delete(firstKey);
+	      }
 
-    return this
-  }
+	      this.map.set(key, value);
+	    }
+
+	    return this
+	  }
+	}
+
+	lrucache = LRUCache;
+	return lrucache;
 }
-
-var lrucache = LRUCache;
 
 var range;
 var hasRequiredRange;
@@ -1287,7 +1295,7 @@ function requireRange () {
 
 	range = Range;
 
-	const LRU = lrucache;
+	const LRU = requireLrucache();
 	const cache = new LRU();
 
 	const parseOptions = parseOptions_1;
@@ -2429,7 +2437,7 @@ function classList(props) {
     'fa-flip': props.flip === true,
     'fa-flip-horizontal': props.flip === 'horizontal' || props.flip === 'both',
     'fa-flip-vertical': props.flip === 'vertical' || props.flip === 'both'
-  }, _defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_classes, "fa-".concat(props.size), props.size !== null), "fa-rotate-".concat(props.rotation), props.rotation !== null), "fa-rotate-by", semver.gte(SVG_CORE_VERSION, ICON_PACKS_STARTING_VERSION) && props.rotateBy), "fa-pull-".concat(props.pull), props.pull !== null), 'fa-swap-opacity', props.swapOpacity), 'fa-bounce', props.bounce), 'fa-shake', props.shake), 'fa-beat', props.beat), 'fa-fade', props.fade), 'fa-beat-fade', props.beatFade), _defineProperty(_defineProperty(_defineProperty(_defineProperty(_classes, 'fa-flash', props.flash), 'fa-spin-pulse', props.spinPulse), 'fa-spin-reverse', props.spinReverse), 'fa-width-auto', semver.gte(SVG_CORE_VERSION, ICON_PACKS_STARTING_VERSION) && props.widthAuto));
+  }, _defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_classes, "fa-".concat(props.size), props.size !== null), "fa-rotate-".concat(props.rotation), props.rotation !== null), 'fa-rotate-by', semver.gte(SVG_CORE_VERSION, ICON_PACKS_STARTING_VERSION) && props.rotateBy), "fa-pull-".concat(props.pull), props.pull !== null), 'fa-swap-opacity', props.swapOpacity), 'fa-bounce', props.bounce), 'fa-shake', props.shake), 'fa-beat', props.beat), 'fa-fade', props.fade), 'fa-beat-fade', props.beatFade), _defineProperty(_defineProperty(_defineProperty(_defineProperty(_classes, 'fa-flash', props.flash), 'fa-spin-pulse', props.spinPulse), 'fa-spin-reverse', props.spinReverse), 'fa-width-auto', semver.gte(SVG_CORE_VERSION, ICON_PACKS_STARTING_VERSION) && props.widthAuto));
   return Object.keys(classes).map(function (key) {
     return classes[key] ? key : null;
   }).filter(function (key) {
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@fortawesome/fontawesome-svg-core'), require('vue')) :
-  typeof define === 'function' && define.amd ? define(['exports', '@fortawesome/fontawesome-svg-core', 'vue'], factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global["vue-fontawesome"] = {}, global.FontAwesome, global.vue));
-})(this, (function (exports, fontawesomeSvgCore, vue) { 'use strict';
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('vue'), require('@fortawesome/fontawesome-svg-core')) :
+  typeof define === 'function' && define.amd ? define(['exports', 'vue', '@fortawesome/fontawesome-svg-core'], factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global["vue-fontawesome"] = {}, global.vue, global.FontAwesome));
+})(this, (function (exports, vue, fontawesomeSvgCore) { 'use strict';
 
   function _arrayLikeToArray(r, a) {
     (null == a || a > r.length) && (a = r.length);
@@ -1029,46 +1029,54 @@
   };
   var coerce_1 = coerce$1;
 
-  class LRUCache {
-    constructor () {
-      this.max = 1000;
-      this.map = new Map();
-    }
+  var lrucache;
+  var hasRequiredLrucache;
 
-    get (key) {
-      const value = this.map.get(key);
-      if (value === undefined) {
-        return undefined
-      } else {
-        // Remove the key from the map and add it to the end
-        this.map.delete(key);
-        this.map.set(key, value);
-        return value
-      }
-    }
+  function requireLrucache () {
+  	if (hasRequiredLrucache) return lrucache;
+  	hasRequiredLrucache = 1;
+  	class LRUCache {
+  	  constructor () {
+  	    this.max = 1000;
+  	    this.map = new Map();
+  	  }
 
-    delete (key) {
-      return this.map.delete(key)
-    }
+  	  get (key) {
+  	    const value = this.map.get(key);
+  	    if (value === undefined) {
+  	      return undefined
+  	    } else {
+  	      // Remove the key from the map and add it to the end
+  	      this.map.delete(key);
+  	      this.map.set(key, value);
+  	      return value
+  	    }
+  	  }
 
-    set (key, value) {
-      const deleted = this.delete(key);
+  	  delete (key) {
+  	    return this.map.delete(key)
+  	  }
 
-      if (!deleted && value !== undefined) {
-        // If cache is full, delete the least recently used item
-        if (this.map.size >= this.max) {
-          const firstKey = this.map.keys().next().value;
-          this.delete(firstKey);
-        }
+  	  set (key, value) {
+  	    const deleted = this.delete(key);
 
-        this.map.set(key, value);
-      }
+  	    if (!deleted && value !== undefined) {
+  	      // If cache is full, delete the least recently used item
+  	      if (this.map.size >= this.max) {
+  	        const firstKey = this.map.keys().next().value;
+  	        this.delete(firstKey);
+  	      }
 
-      return this
-    }
+  	      this.map.set(key, value);
+  	    }
+
+  	    return this
+  	  }
+  	}
+
+  	lrucache = LRUCache;
+  	return lrucache;
   }
-
-  var lrucache = LRUCache;
 
   var range;
   var hasRequiredRange;
@@ -1290,7 +1298,7 @@
 
   	range = Range;
 
-  	const LRU = lrucache;
+  	const LRU = requireLrucache();
   	const cache = new LRU();
 
   	const parseOptions = parseOptions_1;
@@ -2432,7 +2440,7 @@
       'fa-flip': props.flip === true,
       'fa-flip-horizontal': props.flip === 'horizontal' || props.flip === 'both',
       'fa-flip-vertical': props.flip === 'vertical' || props.flip === 'both'
-    }, _defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_classes, "fa-".concat(props.size), props.size !== null), "fa-rotate-".concat(props.rotation), props.rotation !== null), "fa-rotate-by", semver.gte(SVG_CORE_VERSION, ICON_PACKS_STARTING_VERSION) && props.rotateBy), "fa-pull-".concat(props.pull), props.pull !== null), 'fa-swap-opacity', props.swapOpacity), 'fa-bounce', props.bounce), 'fa-shake', props.shake), 'fa-beat', props.beat), 'fa-fade', props.fade), 'fa-beat-fade', props.beatFade), _defineProperty(_defineProperty(_defineProperty(_defineProperty(_classes, 'fa-flash', props.flash), 'fa-spin-pulse', props.spinPulse), 'fa-spin-reverse', props.spinReverse), 'fa-width-auto', semver.gte(SVG_CORE_VERSION, ICON_PACKS_STARTING_VERSION) && props.widthAuto));
+    }, _defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_defineProperty(_classes, "fa-".concat(props.size), props.size !== null), "fa-rotate-".concat(props.rotation), props.rotation !== null), 'fa-rotate-by', semver.gte(SVG_CORE_VERSION, ICON_PACKS_STARTING_VERSION) && props.rotateBy), "fa-pull-".concat(props.pull), props.pull !== null), 'fa-swap-opacity', props.swapOpacity), 'fa-bounce', props.bounce), 'fa-shake', props.shake), 'fa-beat', props.beat), 'fa-fade', props.fade), 'fa-beat-fade', props.beatFade), _defineProperty(_defineProperty(_defineProperty(_defineProperty(_classes, 'fa-flash', props.flash), 'fa-spin-pulse', props.spinPulse), 'fa-spin-reverse', props.spinReverse), 'fa-width-auto', semver.gte(SVG_CORE_VERSION, ICON_PACKS_STARTING_VERSION) && props.widthAuto));
     return Object.keys(classes).map(function (key) {
       return classes[key] ? key : null;
     }).filter(function (key) {
