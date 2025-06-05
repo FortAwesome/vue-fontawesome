716aa6019cda329b49418be8e5509f140a6626db
Jason Lundien <50338269+jasonlundien@users.noreply.github.com>
2025-06-05 09:54:30 -0500

    add some v7 tests - default family styles (#521)

--- src/components/__fixtures__/icons.js
+++ src/components/__fixtures__/icons.js
@@ -1,7 +1,19 @@
-export const faCoffee = {
-  prefix: 'fas',
-  iconName: 'coffee',
-  icon: [640, 512, [], 'f001', '...']
+export const faAlien = {
+  prefix: 'fad',
+  iconName: 'alien',
+  icon: [640, 512, [], 'f003', '...']
+}
+
+export const faBat = {
+  prefix: 'fasds',
+  iconName: 'bat',
+  icon: [640, 512, [], 'f6b5', '...']
+}
+
+export const faCat = {
+  prefix: 'fajdr',
+  iconName: 'cat',
+  icon: [640, 512, [], 'f6b4e', '...']
 }
 
 export const faCircle = {
@@ -10,14 +22,20 @@ export const faCircle = {
   icon: [640, 512, [], 'f002', '...']
 }
 
-export const faAlien = {
-  prefix: 'fad',
-  iconName: 'alien',
-  icon: [640, 512, [], 'f003', '...']
+export const faCoffee = {
+  prefix: 'fas',
+  iconName: 'coffee',
+  icon: [640, 512, [], 'f001', '...']
 }
 
 export const faDog = {
   prefix: 'fass',
   iconName: 'dog',
   icon: [640, 512, [], 'f200', '...']
+}
+
+export const faFish = {
+  prefix: 'fawsb',
+  iconName: 'fish',
+  icon: [640, 512, [], 'f578', '...']
 }
