161a7f6c4537aad94df8ecbb77995c7ec6217c5f
jason <jason@fortawesome.com>
2026-04-15 14:22:06 -0500

    add a helper utility icon

--- src/components/__fixtures__/icons.js
+++ src/components/__fixtures__/icons.js
@@ -40,6 +40,12 @@ export const faFish = {
   icon: [640, 512, [], 'f578', '...']
 }
 
+export const faDogUtility = {
+  prefix: 'faufsb',
+  iconName: 'dog',
+  icon: [640, 512, [], 'f6d4', '...']
+}
+
 export const faDogGraphite = {
   prefix: 'fagt',
   iconName: 'dog',
